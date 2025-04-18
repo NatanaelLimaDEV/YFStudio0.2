aaebe972848a15ceb4a2bf92408917b2c1cf0c64
NatanaelLimaDEV <natanaellimaofc@gmail.com>
2025-04-18 15:20:39 -0300

    adicionado carrosel de avaliacoes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+"use clinet";
+
 import "./home.css";
 import logo from "../../assets/ftUnhas/logo.png";
 import unha1 from "../../assets/ftUnhas/Unha1.png";
@@ -7,7 +9,35 @@ import unha4 from "../../assets/ftUnhas/Unha4.png";
 import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 import { SlLocationPin } from "react-icons/sl";
 
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+
+const reviews = [
+  {
+    name: "Camila Duarte",
+    review:
+      "Simplesmente amei o resultado! Minhas unhas nunca ficaram tão lindas e delicadas. O capricho em cada detalhe do design é impressionante. Com certeza voltarei mais vezes!",
+  },
+  {
+    name: "Juliana Reis",
+    review:
+      "Fiquei encantada com o atendimento e o profissionalismo. O nail design ficou moderno, bem acabado e exatamente como eu pedi. Me senti super bem cuidada!",
+  },
+  {
+    name: "Renata Oliveira",
+    review:
+      "Serviço de altíssima qualidade! A profissional tem mãos de fada e um cuidado incrível com cada etapa. Saí de lá me sentindo renovada e com as unhas dos sonhos.",
+  },
+  {
+    name: "Amanda Lemos",
+    review:
+      "Amei cada detalhe! A profissional foi super atenciosa e caprichosa. Minhas unhas ficaram simplesmente perfeitas, do jeitinho que eu imaginei. Já quero voltar!",
+  },
+];
+
 export default function Home() {
+  const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
+
   return (
     <div className="container">
       <section className="logo">
@@ -54,14 +84,16 @@ export default function Home() {
         </section>
         <section className="reviews">
           <h1>Avaliações</h1>
-          <div>
-            <h4>Camila Duarte</h4>
-            <p>Simplesmente amei o resultado! Minhas unhas nunca ficaram tão lindas e delicadas. O capricho em cada detalhe do design é impressionante. Com certeza voltarei mais vezes!</p>
+          <div className="embla" ref={emblaRef}>
+            <div className="embla__container">
+              {reviews.map((item, index) => (
+                <div className="embla__slide" key={index}>
+                  <h4>{item.name}</h4>
+                  <p>{item.review}</p>
+                </div>
+              ))}
+            </div>
           </div>
-          {/* <div>
-            <h4>Juliana Reis</h4>
-            <p>Fiquei encantada com o atendimento e o profissionalismo. O nail design ficou moderno, bem acabado e exatamente como eu pedi. Me senti super bem cuidada!</p>
-          </div> */}
         </section>
       </div>
       <section className="footer">
